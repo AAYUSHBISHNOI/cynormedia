016dadb7633230d591b9517b1ad24c3203e21ccb
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-02-01 17:40:58 +0530

    Back-To-Top

--- src/Components/Common.jsx/BackToTop.jsx
+++ src/Components/Common.jsx/BackToTop.jsx
@@ -19,12 +19,16 @@ const BackToTop = () => {
 
   return (
     <button
-      className={`fixed bottom-5 right-5 p-2 rounded-full cursor-pointer transition-opacity duration-500 z-50 
+      className={`fixed bottom-1 md:bottom-3 lg:bottom-5 right-1 md:right-3 lg:right-5 p-2 rounded-full cursor-pointer transition-opacity duration-500 z-50 
         bg-gradient-to-b from-[#64ccc5] to-[#1c6B64] border-2 border-white 
         ${isVisible ? "opacity-100" : "opacity-0"}`}
       onClick={scrollToTop}
     >
-      <img src={backtotop} alt="Back to top" className="w-8 h-8" />
+      <img
+        src={backtotop}
+        alt="Back to top"
+        className="w-3 md:w-5 lg:w-8 h-3 md:h-5 lg:h-8"
+      />
     </button>
   );
 };
